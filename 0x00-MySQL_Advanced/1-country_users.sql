6e73a0c7d1db1497c87db51b0b1dc919541c776c
James Alhassan <jamesalhassan052@gmail.com>
2023-07-12 21:53:00 +0000

    Add 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- a table users With these attributes:
+-- id: integer, email: string, name: string, country: ENUM
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
