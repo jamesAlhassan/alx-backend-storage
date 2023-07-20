40c69a97476aec4e60cc1a84ffb1f121cc9a4d3c
James Alhassan <jamesalhassan052@gmail.com>
2023-07-20 04:25:32 +0000

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,11 @@
 '''
 
 import redis
+from typing import Union
+from uuid import uuid4 as uid
+
+
+UnionOfTypes = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -15,3 +20,15 @@ class Cache:
         ' Redis Cache Constructor'
         self._redis = redis.Redis()
         self._redis.flushdb()
+
+    def store(self, data: UnionOfTypes) -> str:
+        '''
+        takes a data argument and returns a string.
+        The method should generate a random key (e.g. using uuid),
+        store the input data in Redis using the random key and
+        return the key.
+        '''
+        key = str(uid())
+        self._redis.mset({key: data})
+
+        return key
